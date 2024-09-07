c4c4679bbbd07f576adbcf0494060f40053c7df8
ramyapatchala <rpatchal@syr.edu>
2024-09-07 15:59:53 -0400

    Update play.py

--- HWs/play.py
+++ HWs/play.py
@@ -1,3 +1,26 @@
+import time
+import os
+import logging
+from dotenv import load_dotenv
+load_dotenv("test.env")
+logging.basicConfig(filename = 'test_models_debug.txt', level=logging.INFO)
+question_to_ask = "Why are LLMs (AI) a danger to society?"
+
+system_message = """
+Goal: Answer the question using bullets.
+      The answer should be appropriate for a 10 year old child to understand
+      """
+
+def output_info(content, start_time, model_info):
+    end_time = time.time()
+    time_taken = end_time - start_time
+    time_taken = round(time_taken * 10) / 10
+
+    output = f"For {model_info}, time taken = " + str(time_taken)
+    logging.info(output)
+    logging.info(f"  --> {content}")
+    st.write(output)
+
 import cohere
 import streamlit as st
 cohere_key = st.secrets['cohere_key']

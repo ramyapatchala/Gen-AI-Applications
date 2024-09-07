6e274db3b31cf289da06d9944762d7b67690e176
ramyapatchala <rpatchal@syr.edu>
2024-09-07 15:51:18 -0400

    Update play.py

--- HWs/play.py
+++ HWs/play.py
@@ -2,13 +2,31 @@ import cohere
 import streamlit as st
 cohere_key = st.secrets['cohere_key']
 
-co = cohere.Client(
-    api_key=cohere_key,
-)
+def do_cohere():
+    cohere_key = st.secrets['cohere_key']
+    co = cohere.Client(cohere_key)
+    response = co.tokenize(
+        text = system_message, model='command')
+    events = co.chat_stream(
+                    model='command-r',
+                    message=question_to_ask,
+                    temperature=0,       
+                    max_tokens=1500,
+                    chat_history=[{"role":"SYSTEM", "message":system_message}],
+                    prompt_truncation='AUTO',
+                    connectors=[],
+                    documents=[]
+    )
 
-chat = co.chat(
-    message="hello world!",
-    model="command"
-)
+    response_text=""
+    for event in events:
+        if event.event_type=="text-generation":
+            response_text = response_text + str(event.text)
+    return response_text
 
-st.write(chat)
+model="Cohere"
+start_time = time.time()
+content = do_cohere()
+output_info(content, start_time, model_info=model)
+st.write("done calcs...")
+    

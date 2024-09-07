24836ab724dc7729270f9473f093c8580ea1b135
ramyapatchala <rpatchal@syr.edu>
2024-09-06 22:33:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ import os
 page1 = st.Page("HW1.py", title="HW 1")
 page2 = st.Page("HW2.py", title="HW 2")
 pg = st.navigation([page1, page2])
-st.set_page_config(page_title="Homework manager")
+st.set_page_config(page_title="HW manager")
 pg.run()

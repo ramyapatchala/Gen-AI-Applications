9cb007df587e72fe3c107f1a665a448bc56526df
ramyapatchala <rpatchal@syr.edu>
2024-09-06 22:40:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from openai import OpenAI
 import os
 
-page1 = st.Page("HW1.py", title="HW 1")
-page2 = st.Page("HW2.py", title="HW 2")
+page1 = st.Page("HWs/HW1.py", title="HW 1")
+page2 = st.Page("HWs/HW2.py", title="HW 2")
 pg = st.navigation([page1, page2])
-st.set_page_config(page_title="HW manager")
+st.set_page_config(page_title="HW Manager")
 pg.run()

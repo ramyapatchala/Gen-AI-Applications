fde08fe45bcfd405fa3d61e8cdddb351088ec4a0
ramyapatchala <rpatchal@syr.edu>
2024-09-07 19:32:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import os
 page1 = st.Page("HWs/HW1.py", title="HW 1")
 page2 = st.Page("HWs/HW2.py", title="HW 2")
 page3 = st.Page("HWs/play.py", title="play")
-pg = st.navigation([page1, page2, page3])
+page4 = st.Page("HWs/claude.py", title="claude")
+
+pg = st.navigation([page1, page2, page3, page4])
 st.set_page_config(page_title="HW Manager")
 pg.run()

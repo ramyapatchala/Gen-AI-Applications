6b54bc08c712bcca0249de8fc0bc48e8ff3f5339
ramyapatchala <rpatchal@syr.edu>
2024-09-12 19:42:34 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import os
 
 page1 = st.Page("HWs/HW1.py", title="HW 1")
 page2 = st.Page("HWs/HW2.py", title="HW 2")
-page3 = st.page("HWs/HW3.py", title="HW 3")
+page3 = st.Page("HWs/HW3.py", title="HW 3")
 
 pg = st.navigation([page1, page2, page3])
 st.set_page_config(page_title="HW Manager")

69aa86c75749da7e3b7a1c1d26b46bbbdafb7ebe
ramyapatchala <rpatchal@syr.edu>
2024-10-23 17:38:19 -0400

    Update HW6.py

--- HWs/HW6.py
+++ HWs/HW6.py
@@ -82,6 +82,8 @@ def find_most_interesting_news():
             include=['documents', 'metadatas'],
             n_results=3  # Adjust this number as needed
         )
+
+        st.write(results)
         return results
     else:
         st.error("VectorDB not set up. Please set up the VectorDB first.")

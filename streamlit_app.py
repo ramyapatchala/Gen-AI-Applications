7a577d8a0cb3baba083291659733a529c9f8994c
ramyapatchala <rpatchal@syr.edu>
2024-12-27 09:22:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 from openai import OpenAI
 import os
 
-page1 = st.Page("HWs/HW1.py", title="HW 1: Document Q&A System")
-page2 = st.Page("HWs/HW2.py", title="HW 2: Multi Webpage Summarizer")
-page3 = st.Page("HWs/HW3.py", title="HW 3: Chatbot")
-page4 = st.Page("HWs/HW4.py", title="HW 4: Retreived Augmented Generation (RAG)")
-page5 = st.Page("HWs/HW5.py", title="HW 5: iSchool Club Chatbot")
-page6 = st.Page("HWs/HW6.py", title = "HW 6: News Bot")
+page1 = st.Page("Document Q&A System.py", title="Document Q&A System")
+page2 = st.Page("Multi Webpage Summarizer.py", title="Multi Webpage Summarizer")
+page3 = st.Page("Chatbot.py", title="Chatbot")
+page4 = st.Page("Retreived Augmented Generation (RAG).py", title="Retreived Augmented Generation (RAG)")
+page5 = st.Page("iSchool Club Agent.py", title="iSchool Club Agent")
+page6 = st.Page("News Bot.py", title = "News Bot")
 
 pg = st.navigation([page1, page2, page3, page4, page5, page6])
-st.set_page_config(page_title="HW Manager")
+st.set_page_config(page_title="Gen AI Applications")
 pg.run()
